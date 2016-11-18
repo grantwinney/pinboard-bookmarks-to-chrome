2891d9a4d03f26669c348011382eb303a42fea31
Grant Winney <grantwinney@gmail.com>
2016-11-17 19:00:44 -0500

    sample storage code.. gonna need that later

--- options.js
+++ options.js
@@ -312,3 +312,12 @@ function makePinboardApiRequest() {
 // };
 //
 // login('CF100DA71C020A2BC0DF');
+
+// chrome.storage.sync.set({'value': theValue}, function() {
+//           // Notify that we saved.
+//           message('Settings saved');
+//         });
+
+// StorageArea.get(string or array of string or object keys, function callback)
+
+// https://developer.chrome.com/extensions/storage
