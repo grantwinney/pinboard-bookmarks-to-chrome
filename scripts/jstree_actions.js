147370f1ee6fd314e79c0069ce89e70a8d607088
Grant Winney <grantwinney@gmail.com>
2016-11-26 08:11:41 -0500

    Removed unused code.

--- scripts/jstree_actions.js
+++ scripts/jstree_actions.js
@@ -59,39 +59,3 @@ function jstree_node_delete() {
 
 	tagTree.delete_node(firstNodeId);
 };
-
-// $(function () {
-// 	var to = false;
-// 	$('#demo_q').keyup(function () {
-// 		if(to) { clearTimeout(to); }
-// 		to = setTimeout(function () {
-// 			var v = $('#demo_q').val();
-// 			$('#jstree_demo').jstree(true).search(v);
-// 		}, 250);
-// 	});
-//
-// 	$('#jstree_demo')
-// 		.jstree({
-// 			"core" : {
-// 				"animation" : 0,
-// 				"check_callback" : true,
-// 				'force_text' : true,
-// 				"themes" : { "stripes" : true },
-// 				'data' : {
-// 					'url' : function (node) {
-// 						return node.id === '#' ? '/static/3.3.3/assets/ajax_demo_roots.json' : '/static/3.3.3/assets/ajax_demo_children.json';
-// 					},
-// 					'data' : function (node) {
-// 						return { 'id' : node.id };
-// 					}
-// 				}
-// 			},
-// 			"types" : {
-// 				"#" : { "max_children" : 1, "max_depth" : 4, "valid_children" : ["root"] },
-// 				"root" : { "icon" : "/static/3.3.3/assets/images/tree_icon.png", "valid_children" : ["default"] },
-// 				"default" : { "valid_children" : ["default","file"] },
-// 				"file" : { "icon" : "glyphicon glyphicon-file", "valid_children" : [] }
-// 			},
-// 			"plugins" : [ "contextmenu", "dnd", "search", "state", "types", "wholerow" ]
-// 		});
-// });
