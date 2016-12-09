da2ec49845f9dc172f7ac9c08ec924103ebf12a9
Grant Winney <grantwinney@gmail.com>
2016-12-09 08:35:53 -0500

    Refactoring multiple files back into one. Various fixes.

--- scripts/options.js
+++ scripts/options.js
@@ -4,32 +4,411 @@ ALL_POSTS_REQUEST_LIMIT_IN_SEC = 300;
 ROOT_NODE_ID = 'node_0';
 
 function disableInputElements(message) {
-    $("#tagTree").addClass("disabledElement");
-    $("#apiToken").addClass("disabledElement");
-    $("#verifyApiToken").addClass("disabledElement");
-    $("#generateBookmarks").addClass("disabledElement");
-    $("#tagContainer").addClass("disabledElement");
-    $("#innerNotice").text(message);
+    ['#tagTree','#apiToken','#verifyApiToken','#generateBookmarks','#tagContainer'].forEach(function(element) {
+        $(element).addClass('disabledElement');
+    });
+    $("#innerNotice").text(message + '...');
 }
 
 function enableInputElements() {
-    $("#tagTree").removeClass("disabledElement");
-    $("#apiToken").removeClass("disabledElement");
-    $("#verifyApiToken").removeClass("disabledElement");
-    $("#generateBookmarks").removeClass("disabledElement");
-    $("#tagContainer").removeClass("disabledElement");
+    ['#tagTree','#apiToken','#verifyApiToken','#generateBookmarks','#tagContainer'].forEach(function(element) {
+        $(element).removeClass("disabledElement");
+    });
     $("#innerNotice").text('');
 }
 
-window.addEventListener('load', function load(event) {
-    disableInputElements("Initializing...");
-    try {
-        loadApiTokenFromStorageAndVerify();
-        loadSelectedTagsFromStorage();
-        registerCheckApiTokenButton();
-        registerGenerateBookmarksButton();
-    }
-    finally {
+
+/***********************************
+ LOADING AND MANIPULATING TAG DATA
+************************************/
+
+function retrieveAndDisplayAllTags() {
+    chrome.storage.local.get('api_token', function(result) {
+        if (result != undefined && result.api_token != undefined && $.trim(result.api_token) != '') {
+            var client = new XMLHttpRequest();
+            client.open("GET", 'https://api.pinboard.in/v1/tags/get?format=json&auth_token=' + result.api_token);
+            client.onload = function(e) {
+                if (client.status == 200) {
+                    disableInputElements("Loading Tags from Pinboard");
+                    $("#tagContainer").empty();
+                    JSON.parse(client.responseText, (tagName, tagCount) =>
+                    {
+                        if (tagName != '') {
+                            var element = document.createElement('button');
+                            element.textContent = tagName;
+                            element.addEventListener('click', function() {
+                                jstree_node_create(tagName);
+                            });
+                            element.classList.add("tag");
+                            $("#tagContainer").append(element);
+                        }
+                    });
+                    enableInputElements();
+                } else {
+                    logInvalidResponse('/tags/get', client);
+                }
+            };
+            client.send();
+        } else {
+            enableInputElements();
+        }
+    });
+}
+
+function loadSelectedTagsFromStorage() {
+    chrome.storage.sync.get('selected_tags', function(result) {
+        if (result != undefined && result.selected_tags != undefined) {
+            var data = result.selected_tags;
+        } else {
+            var data = [ { "id" : ROOT_NODE_ID, "text" : "Pinboard", "icon" : "images/root.gif" } ];
+        }
+
+        $('#tagTree').jstree({
+            'core' : {
+                'animation' : 100,
+                'themes' : { 'stripes' : false },
+                'multiple' : false,
+                'check_callback' : true,
+                'data' : data,
+            },
+            'types' : {
+                '#' : {
+                    'max_children' : 1,
+                    'valid_children' : ['root']
+                },
+                'root' : {
+                    'icon' : '../../images/tree_icon.png',
+                    'valid_children' : ['default']
+                },
+                'default' : {
+                    'valid_children' : ['default', 'file']
+                },
+                'file' : {
+                    'icon' : '../../images/bookmark_icon.png',
+                    'valid_children' : []
+                }
+            },
+            'plugins' : [
+                'contextmenu', 'dnd', 'sort',
+                'state', 'types', 'wholerow'
+            ]
+        });
+
         enableInputElements();
+    });
+}
+
+function getSelectedTagDataJson() {
+    return $('#tagTree').jstree(true).get_json('#');
+}
+
+function getDistinctTagNames() {
+    var names = getAllNamesFromTagTree(getSelectedTagDataJson());
+    return names.filter((v, i, a) => a.indexOf(v) === i);
+}
+
+function getAllNamesFromTagTree(treeNode) {
+    var childNodeText = [];
+    for (var i = 0; i < treeNode.length; i++) {
+        if (treeNode[i]['type'] == 'default') {
+            childNodeText = childNodeText.concat(getAllNamesFromTagTree(treeNode[i]["children"]));
+        } else {
+            childNodeText.push(treeNode[i]["text"]);
+        }
     }
+    return childNodeText;
+}
+
+/***********************
+ JSTREE NODE FUNCTIONS
+************************/
+
+function jstree_node_create(nodeName = '') {
+	var tagTree = $('#tagTree').jstree();
+    var selectedNodeIds = tagTree.get_selected();
+    var firstNodeId = selectedNodeIds.length ? selectedNodeIds[0] : ROOT_NODE_ID;
+
+    var firstNode = tagTree.get_node(firstNodeId);
+    if (firstNode.type == 'default') {
+        firstNode.state.opened = true;
+    }
+    else {
+        firstNodeId = tagTree.get_node(firstNode).parent;
+    }
+
+    if (nodeName == '') {
+	    newNodeId = tagTree.create_node(firstNodeId, {"id" : "", "text" : "New Folder", "opened" : true});
+        tagTree.edit(newNodeId);
+    }
+    else {
+        tagTree.create_node(firstNodeId, {"id" : "", "type" : "file", "text" : nodeName});
+    }
+};
+
+// function jstree_node_rename() {
+// 	var tagTree = $('#tagTree').jstree();
+//     var selectedNodeIds = tagTree.get_selected();
+// 	if (!selectedNodeIds.length) {
+//         return false;
+//     }
+//
+// 	var firstNodeId = selectedNodeIds[0];
+//     var firstNode = tagTree.get_node(firstNodeId);
+//     if (firstNode.type == 'file') {
+//         return false;
+//     }
+//
+// 	tagTree.edit(firstNodeId);
+// };
+
+// function jstree_node_delete() {
+// 	var tagTree = $('#tagTree').jstree();
+//     var selectedNodeIds = tagTree.get_selected();
+// 	if (!selectedNodeIds.length) {
+//         return false;
+//     }
+//
+// 	var firstNodeId = selectedNodeIds[0];
+//     if (firstNodeId == ROOT_NODE_ID) {
+//         return false;
+//     }
+//
+//     var firstNode = tagTree.get_node(firstNodeId);
+//     if (firstNode.type == 'default' && firstNode.children.length > 0) {
+//         if (!(confirm('Are you sure you want to delete this folder and all subfolders and bookmarks within it?'))) {
+//             return false;
+//         }
+//     }
+//
+// 	tagTree.delete_node(firstNodeId);
+// };
+
+
+/****************************************
+ GET/SET AND VERIFICATION FOR API TOKEN
+*****************************************/
+
+function loadApiTokenFromStorageAndVerify() {
+    chrome.storage.local.get('api_token', function(result) {
+        if (result != undefined && result.api_token != undefined) {
+            $("#apiToken").val(result.api_token);
+            verifyApiTokenAndLoadTags(result.api_token);
+        } else {
+            setApiTokenValidityIcon(false);
+            enableInputElements();
+        }
+    });
+}
+
+function verifyApiTokenAndLoadTags(apiToken) {
+    var client = new XMLHttpRequest();
+    client.open("GET", 'https://api.pinboard.in/v1/user/api_token?format=json&auth_token=' + apiToken);
+    client.onload = function(e) {
+        setApiTokenValidityIcon(client.status == 200);
+        if (client.status == 200) {
+            chrome.storage.local.set({'api_token': apiToken});
+            retrieveAndDisplayAllTags();
+        } else {
+            logInvalidResponse('/user/api_token', client, false);
+        }
+    };
+    if ($.trim($("#apiToken").val()) != '') {
+        disableInputElements("Verifying API Token");
+        client.send();
+    }
+}
+
+function setApiTokenValidityIcon(isTokenValid) {
+    var ind = document.getElementById('apiTokenStatusIndicator');
+    if (isTokenValid) {
+        ind.src = 'images/check.png';
+        ind.title = 'Valid Auth Token';
+    } else {
+        ind.src = 'images/wrong.png';
+        ind.title = 'Invalid Auth Token';
+    }
+}
+
+/****************************
+ ERROR HANDLING AND LOGGING
+*****************************/
+
+function logInvalidResponse(action, client, showPopup = true) {
+    var errorMessage = 'Call to ' + action + ' failed with response: ' + client.status + ' ' + client.statusText;
+    console.error(errorMessage);
+    if (showPopup)
+        alert(errorMessage);
+    enableInputElements();
+}
+
+function logError(message, showPopup = false) {
+    var errorMessage = 'An error has occurred: ' + message;
+    console.error(errorMessage);
+    if (showPopup)
+        alert(errorMessage);
+    enableInputElements();
+}
+
+window.onerror = function(messageOrEvent, sourceUrl, lineNo, columnNo, error) {
+    var errorMessage = 'An error occurred on "' + sourceUrl + '[' + lineNo + ':' + columnNo + ']": ' + messageOrEvent;
+    console.error(errorMessage);
+    console.error('Stack trace: ' + error.stack);
+    alert(errorMessage);
+    enableInputElements();
+    return false;
+}
+
+// window.addEventListener('error', function load(event) {
+//     console.error('An error has occurred: ' + event.error.toString());
+//     console.error('Stack trace: ' + event.error.stack);
+//     enableInputElements();
+// });
+
+
+/***************************************************************
+ GET BOOKMARK DATA FROM PINBOARD AND LOCAL BOOKMARK GENERATION
+****************************************************************/
+
+function getAllPostsAndGenerateBookmarks() {
+    chrome.storage.local.get('all_bookmarks_last_updated', function(result) {
+        if (result != undefined
+            && result.all_bookmarks_last_updated != undefined
+            && ((new Date() - Date.parse(result.all_bookmarks_last_updated)) / 1000) < ALL_POSTS_REQUEST_LIMIT_IN_SEC) {
+            getAllPostsFromLocalStorage();
+        } else {
+            getAllPostsFromPinboard();
+        }
+    });
+}
+
+function getAllPostsFromLocalStorage() {
+    chrome.storage.local.get('all_bookmarks', function(result) {
+        if (result != undefined && result.all_bookmarks != undefined) {
+            generateBookmarks(result.all_bookmarks);
+        } else {
+            getAllPostsFromPinboard();
+        }
+    });
+}
+
+function getAllPostsFromPinboard() {
+    chrome.storage.local.get('api_token', function(result) {
+        if (result != undefined && result.api_token != undefined) {
+            var client = new XMLHttpRequest();
+            client.open("GET", 'https://api.pinboard.in/v1/posts/all?format=json&auth_token=' + result.api_token);
+            client.onload = function(e) {
+                if (client.status == 200) {
+                    var rawdata = JSON.parse(client.responseText);
+                    var data = [];
+                    rawdata.forEach(function(url) {
+                        var o = new Object();
+                        o.href = url['href'];
+                        o.description = url['description'];
+                        o.tags = url['tags'].split(' ');
+                        data.push(o);
+                    });
+                    chrome.storage.local.set({'all_bookmarks_last_updated': new Date().toString()});
+                    chrome.storage.local.set({'all_bookmarks': data});
+                    generateBookmarks(data);
+                } else {
+                    logInvalidResponse('/posts/all', client);
+                }
+            };
+            client.send();
+        } else {
+            var message = 'API Token is missing.\n\nUnable to get bookmarks from Pinboard.';
+            alert(message);
+            logError(message);
+        }
+    });
+}
+
+function generateBookmarks(allUrls) {
+    chrome.bookmarks.getChildren("0", function(children) {
+        var isBarFound = false;
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].title == 'Bookmarks Bar') {
+                isBarFound = true;
+                var relevantUrls = filterBookmarksToSelectedTags(allUrls);
+                var selectedTags = getSelectedTagDataJson();
+                // if (ADD_ALL_TO_SUBFOLDER) {
+                    createPageOrFolder(children[i].id, selectedTags, relevantUrls);
+                // } else {
+                //     // call createPageOrFolder() in such a way that the main Pinboard folder is not created,
+                //     // and everything else appears directly in the Bookmarks Bar instead of the Pinboard folder.
+                // }
+                break;
+            }
+        }
+        if (isBarFound == false) {
+            logError('Cannot add bookmarks. Unable to find the \'Bookmarks Bar\'.')
+        }
+    });
+}
+
+function filterBookmarksToSelectedTags(allUrls) {
+    var distinctTagNames = getDistinctTagNames();
+    var relevantUrls = [];
+    distinctTagNames.forEach(function(tag) {
+        allUrls.forEach(function(url) {
+            if (url.tags.indexOf(tag) != -1 && relevantUrls.indexOf(url) == -1) {
+                relevantUrls.push(url);
+            }
+        })
+    })
+    return relevantUrls;
+}
+
+function createPageOrFolder(parentNodeId, tagNode, urls) {
+    tagNode.forEach(function(tag) {
+        if (tag['type'] == 'default') {
+            chrome.bookmarks.create({'parentId': parentNodeId,
+                                     'title': tag['text']},
+                                    function(newFolder) {
+                                        createPageOrFolder(newFolder.id, tag['children'], urls);
+                                    });
+        } else {
+            urls.forEach(function(url) {
+                if (url['tags'].indexOf(tag['text']) != -1) {
+                    chrome.bookmarks.create({'parentId': parentNodeId,
+                                             'title': url['description'],
+                                             'url': url['href']});
+                }
+            })
+        }
+    })
+    enableInputElements();
+}
+
+
+/*********************
+ EVENT SUBSCRIPTIONS
+**********************/
+
+function subscribeEvents() {
+    $("#verifyApiToken").on("click", function() {
+        verifyApiTokenAndLoadTags($("#apiToken").val());
+    });
+
+    $("#generateBookmarks").on('click', function() {
+        disableInputElements("Generating Bookmarks... this may take a minute");
+        chrome.storage.sync.set({'selected_tags': $('#tagTree').jstree(true).get_json('#')}, function() {
+            getAllPostsAndGenerateBookmarks();
+        });
+    });
+
+    $("#debug").on('click', function() {
+        if (confirm('Delete all stored data for this extension?')) {
+            chrome.storage.local.clear();
+            chrome.storage.sync.clear();
+            window.location.reload();
+        }
+    });
+}
+
+window.addEventListener('load', function load(event) {
+    disableInputElements("Initializing");
+    loadSelectedTagsFromStorage();
+    loadApiTokenFromStorageAndVerify();
+    subscribeEvents();
 });
